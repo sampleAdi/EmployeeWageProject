3db26e5f385d21a632cd359def863d54cdb9942e
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-20 14:37:54 +0530

    Project upsated

---
+++ CompleteProjectCode.js
@@ -0,0 +1,9 @@
+const present = 1;
+
+let isPresent = Math.floor(Math.random() * 10) % 2;
+
+if (isPresent == present) {
+  console.log("Employee is Present");
+} else {
+  console.log("Employee is not Present");
+}
