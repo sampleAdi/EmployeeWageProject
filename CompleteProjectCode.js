420318bc690c35c8b4aa062140da4e02ab97711b
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-21 10:32:46 +0530

    Project updated

--- CompleteProjectCode.js
+++ CompleteProjectCode.js
@@ -1,3 +1,4 @@
+//UC-1 checking
 const present = 1;
 
 let isPresent = Math.floor(Math.random() * 10) % 2;
@@ -6,9 +7,10 @@ if (isPresent == present) {
   console.log("Employee is Present");
 } else {
   console.log("Employee is not Present");
+  return;
 }
 
-//calculating dailywage of an employee with using a function
+//UC-2 and UC-3 calculating dailywage of an employee with using a function
 let workingHours = Math.floor(Math.random() * 10) % 3;
 
 const isPartTime = 1;
@@ -34,7 +36,7 @@ let wage = wagePerHour * getWorkingHours(workingHours);
 
 console.log("Daily wage generated for employee is ", wage);
 
-//calcultating wages for a Month
+//UC-4 calcultating wages for a Month
 
 const numberOfWorkingDays = 20;
 
@@ -50,7 +52,7 @@ console.log(
   "Monthly Wage of the Employee for hours " + empHrs + " is " + empWage
 );
 
-//calculating Employee wage Untill hours are 160 or days completed are 20
+//UC-5 calculating Employee wage Untill hours are 160 or days completed are 20
 
 empHrs = 0;
 let numberOfTotalDays = 0;
@@ -66,6 +68,42 @@ console.log(
     numberOfTotalDays +
     ", Total working hours are : " +
     empHrs +
+    ", Wage of Employee is : " +
+    empWage
+);
+
+//UC-6 storing the daily wage in an array and also calculating totalWage
+
+function getWage(empHrs) {
+  return empHrs * wagePerHour;
+}
+
+const maxWorkingHrs = 160;
+const maxWorkingDays = 20;
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let dailyWages = new Array();
+
+numberOfTotalDays = 0;
+
+while (totalEmpHrs <= maxWorkingHrs && numberOfTotalDays < maxWorkingDays) {
+  empCheck = Math.floor(Math.random() * 10) % 3;
+  empHrs = getWorkingHours(empCheck);
+
+  totalEmpHrs += empHrs;
+
+  dailyWages.push(getWage(empHrs));
+
+  numberOfTotalDays++;
+}
+
+empWage = totalEmpHrs * wagePerHour;
+console.log(
+  "Total working days are : " +
+    numberOfTotalDays +
+    ", Total working hours are : " +
+    totalEmpHrs +
     ", Wage of Employee is : " +
     empWage
 );
